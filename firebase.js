d8c80f6e4c383e1bda5a1856ad3f87d6f6dc469c
김은식 <gamrom@MacBook-Pro.local>
2023-12-13 17:12:27 +0900

    ignore셋

--- firebase.js
+++ firebase.js
@@ -1,6 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+
+//vercel에서 확인하시오
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
