7b10dfb8879d7786adb268a949abc028cb155ec6
김은식 <gamrom@MacBook-Pro.local>
2023-12-16 14:00:13 +0900

    drawer수정중

--- app/header.tsx
+++ app/header.tsx
@@ -7,8 +7,66 @@ import logo from '../public/logo.png'
 import { useEffect, useState } from 'react'
 import { auth } from '../firebase'
 
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import MenuIcon from '@mui/icons-material/Menu';
+
+
+
 export const Header = () => {
   const [isLogin, setIsLogin] = useState<boolean>(false);
+
+  const [state, setState] = useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+  const toggleDrawer = (anchor: any, open: any) => (event: any) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor: any) => (
+    <Box
+      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+       <div>회원가입</div>
+      </List>
+      <Divider />
+      <List>
+        {/* {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))} */}
+        <div>
+          리스트1
+        </div>
+      </List>
+    </Box>
+  );
 
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
@@ -56,6 +114,19 @@ export const Header = () => {
           <Button variant="text" type="button" onClick={() => signOut()}>로그아웃</Button>
         )}
       </div>
+
+
+      <div key={"right"}>
+        <Button onClick={toggleDrawer("right", true)}>{"right"}</Button>
+        <Drawer
+          anchor={"right"}
+          open={state["right"]}
+          onClose={toggleDrawer("right", false)}
+        >
+          {list("right")}
+        </Drawer>
+      </div>
+
     </div>
   )
 }
