9f73c0891febad4ab8852d518a865c80a7a12f57
김은식 <gamrom@MacBook-Pro.local>
2023-12-26 18:48:45 +0900

    가독성, title 변경

--- app/style.tsx
+++ app/style.tsx
@@ -3,9 +3,12 @@ export const modalStyle = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  maxWidth: "500px",
   bgcolor: "#FFFFFF",
   border: '2px solid #000',
   boxShadow: 24,
-  p: 4,
+  p: 2,
+  maxWidth: "80%",
+  width: "100%", /* Ensures the modal takes 100% width within the max-width limit */
+  maxHeight: "80vh", /* Restricts the maximum height to 80% of the viewport height */
+  overflowY: "auto",
 };
