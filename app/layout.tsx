a5c48fc5f34d73f305c72d1f71167f89b139663b
김은식 <gamrom@MacBook-Pro.local>
2024-01-02 00:09:07 +0900

    og image, favicon 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,9 +25,25 @@ export default function RootLayout({
       <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale='ko'>
         <head>
           <link rel="icon" href="/favicon.ico" sizes="any" />
+
+          <title>감롬의보드게임동아리</title>
+          <meta name="description" content="감보동 사이트입니다." />
+
+          <meta property="og:url" content="https://www.gambodong.com/" />
+          <meta property="og:type" content="website" />
           <meta property="og:title" content="감롬의보드게임동아리" />
-          <meta property="og:image" content="<generated>" />
-          <title>감롬의 보드게임 동아리</title>
+          <meta property="og:description" content="" />
+          <meta property="og:image" content="https://opengraph.b-cdn.net/production/documents/e11d93e3-ddd4-4b19-a19a-14592b5cb9ea.png?token=JBsz2BlhZcfuovakmojI0n6WZjcnnWJmCGWTxg4e0dY&height=400&width=800&expires=33240121561" />
+
+
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta property="twitter:domain" content="gambodong.com" />
+          <meta property="twitter:url" content="https://www.gambodong.com/" />
+          <meta name="twitter:title" content="감롬의보드게임동아리" />
+          <meta name="twitter:description" content="" />
+          <meta name="twitter:image" content="https://opengraph.b-cdn.net/production/documents/e11d93e3-ddd4-4b19-a19a-14592b5cb9ea.png?token=JBsz2BlhZcfuovakmojI0n6WZjcnnWJmCGWTxg4e0dY&height=400&width=800&expires=33240121561" />
+
+
         </head>
         <body className={`${inter.className} max-w-[650px] mx-auto px-4 min-h-screen`}>
           <Header />
