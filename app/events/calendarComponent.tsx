8e738d2e1d11cac0cf2e90cda45a003f2ce758e2
김은식 <gamrom01@naver.com>
2024-06-01 11:02:18 +0900

    보유보드게임삭제, 번개생성버튼 커서

--- app/events/calendarComponent.tsx
+++ app/events/calendarComponent.tsx
@@ -1,28 +1,28 @@
-'use client'
-import { ToggleButton, ToggleButtonGroup } from '@mui/material';
-import { DateCalendar } from '@mui/x-date-pickers';
-import { useEffect, useState } from 'react';
-import { Button } from "@material-tailwind/react"
-import Link from 'next/link';
-import dayjs, { Dayjs } from 'dayjs';
-import { getCurrentMonthEvents, getApplyEvent, getEvents } from '../api';
-import { DayCalendarSkeleton } from '@mui/x-date-pickers/DayCalendarSkeleton';
-import { Badge } from '@mui/material';
-import { PickersDay, PickersDayProps } from '@mui/x-date-pickers/PickersDay';
-import { LoadingComp } from '../loadingComp';
-import { useGetCurrentUser } from '../hooks/useGetCurrentUser';
-import { useSearchParams } from 'next/navigation';
+"use client";
+import { ToggleButton, ToggleButtonGroup } from "@mui/material";
+import { DateCalendar } from "@mui/x-date-pickers";
+import { useEffect, useState } from "react";
+import { Button } from "@material-tailwind/react";
+import Link from "next/link";
+import dayjs, { Dayjs } from "dayjs";
+import { getCurrentMonthEvents, getApplyEvent, getEvents } from "../api";
+import { DayCalendarSkeleton } from "@mui/x-date-pickers/DayCalendarSkeleton";
+import { Badge } from "@mui/material";
+import { PickersDay, PickersDayProps } from "@mui/x-date-pickers/PickersDay";
+import { LoadingComp } from "../loadingComp";
+import { useGetCurrentUser } from "../hooks/useGetCurrentUser";
+import { useSearchParams } from "next/navigation";
 
 type EventProps = {
-  id: number,
-  start_time: any,
-  end_time: any,
-  title: string
-  location: string,
-  owner_name: string,
-  max_members_count: number,
-  current_members_count: number,
-}
+  id: number;
+  start_time: any;
+  end_time: any;
+  title: string;
+  location: string;
+  owner_name: string;
+  max_members_count: number;
+  current_members_count: number;
+};
 
 export const CalendarComponent = () => {
   const [toggleFilter, setToggleFilter] = useState<string | null>("monthAll");
@@ -32,13 +32,12 @@ export const CalendarComponent = () => {
 
   const handleFilter = (
     event: React.MouseEvent<HTMLElement>,
-    newToggle: string | null,
+    newToggle: string | null
   ) => {
-    setToggleFilter(newToggle)
+    setToggleFilter(newToggle);
   };
 
   const [events, setEvents] = useState<EventProps[]>([]);
-
 
   const [callendarEvents, setCallendarEvents] = useState<Array<number>>([]);
   useEffect(() => {
@@ -53,68 +52,75 @@ export const CalendarComponent = () => {
         const filteredData = allEvents.filter((event: EventProps) => {
           const startTime = dayjs(event.start_time.replace(/-/g, "/"));
           const endTime = dayjs(event.end_time.replace(/-/g, "/"));
-          return startTime.isBefore(pickDate.endOf('day')) && endTime.isAfter(pickDate.startOf('day'));
-        })
+          return (
+            startTime.isBefore(pickDate.endOf("day")) &&
+            endTime.isAfter(pickDate.startOf("day"))
+          );
+        });
         setEvents(filteredData);
       });
     }
-  }, [pickDate])
+  }, [pickDate]);
 
   useEffect(() => {
-
     if (toggleFilter === "monthAll") {
       setToggleFilter("monthAll");
       getCurrentMonthEvents({
         year: pickDate ? String(pickDate.year()) : String(dayjs().year()),
-        month: pickDate ? String(pickDate.month() + 1) : String(dayjs().month() + 1),
+        month: pickDate
+          ? String(pickDate.month() + 1)
+          : String(dayjs().month() + 1),
       }).then((res) => {
         setEvents(res.data);
-      })
+      });
     } else if (toggleFilter === "canJoin") {
       setToggleFilter("canJoin");
       getEvents().then((res) => {
         //res.data 중에서 시작시간이 오늘 이상인 이벤트들을 뽑는다.
         const filter1 = res.data.filter((event: EventProps) => {
           const startTime = dayjs(event.start_time.replace(/-/g, "/"));
-          return startTime.isAfter(dayjs().startOf('day'));
-        })
+          return startTime.isAfter(dayjs().startOf("day"));
+        });
 
         const filter2 = filter1.filter((event: EventProps) => {
           return event.current_members_count !== event.max_members_count;
-        })
+        });
 
         setEvents(filter2);
-      })
+      });
     } else if (toggleFilter === "alreadyJoin") {
       setToggleFilter("alreadyJoin");
       getApplyEvent().then((res) => {
         setEvents(res.data);
-      })
+      });
     }
 
     setEventsIsLoading(false);
-  }, [toggleFilter, pickDate, callendarEvents])
+  }, [toggleFilter, pickDate, callendarEvents]);
 
   useEffect(() => {
     //모든 이벤트의 시작날짜와 끝날짜 사이의 모든 날짜를 구한다.
     const allDays: number[] = [];
     getCurrentMonthEvents({
       year: pickDate ? String(pickDate.year()) : String(dayjs().year()),
-      month: pickDate ? String(pickDate.month() + 1) : String(dayjs().month() + 1),
+      month: pickDate
+        ? String(pickDate.month() + 1)
+        : String(dayjs().month() + 1),
     }).then((res) => {
       const allEvents = res.data;
-      allEvents && allEvents.forEach((event: EventProps) => {
-        const startDay = dayjs(event.start_time.replace(/-/g, "/")).date();
-        const endDay = dayjs(event.end_time.replace(/-/g, "/")).date();
-        for (let i = startDay; i <= endDay; i++) {
-          allDays.push(i);
-        }
-      })
+      allEvents &&
+        allEvents.forEach((event: EventProps) => {
+          const startDay = dayjs(event.start_time.replace(/-/g, "/")).date();
+          const endDay = dayjs(event.end_time.replace(/-/g, "/")).date();
+          for (let i = startDay; i <= endDay; i++) {
+            allDays.push(i);
+          }
+        });
       //중복된 날짜를 제거한다.
       const uniqueDays = Array.from(new Set(allDays));
       setCallendarEvents(uniqueDays);
-    })
-  }, [pickDate])
+    });
+  }, [pickDate]);
 
   const queryPickDate = useSearchParams().get("pickDate");
   useEffect(() => {
@@ -124,20 +130,28 @@ export const CalendarComponent = () => {
     } else {
       setPickDate(null);
     }
-  }, [queryPickDate])
+  }, [queryPickDate]);
 
-  function ServerDay(props: PickersDayProps<Dayjs> & { callendarEvents?: number[] }) {
+  function ServerDay(
+    props: PickersDayProps<Dayjs> & { callendarEvents?: number[] }
+  ) {
     const { callendarEvents = [], day, outsideCurrentMonth, ...other } = props;
 
-    const isSelected = !props.outsideCurrentMonth && callendarEvents.indexOf(props.day.date()) >= 0;
+    const isSelected =
+      !props.outsideCurrentMonth &&
+      callendarEvents.indexOf(props.day.date()) >= 0;
 
     return (
       <Badge
         key={props.day.toString()}
         overlap="circular"
-        badgeContent={isSelected ? '🔴' : undefined}
+        badgeContent={isSelected ? "🔴" : undefined}
       >
-        <PickersDay {...other} outsideCurrentMonth={outsideCurrentMonth} day={day} />
+        <PickersDay
+          {...other}
+          outsideCurrentMonth={outsideCurrentMonth}
+          day={day}
+        />
       </Badge>
     );
   }
@@ -151,42 +165,54 @@ export const CalendarComponent = () => {
         size="small"
         className="w-full mt-4"
       >
-        <ToggleButton className="w-full text-xs" color="secondary" value="monthAll" aria-label="left aligned">
+        <ToggleButton
+          className="w-full text-xs"
+          color="secondary"
+          value="monthAll"
+          aria-label="left aligned"
+        >
           이번달 모든 번개
         </ToggleButton>
-        {
-          currentUser && currentUser.data.role !== "guest" && (
-            <ToggleButton className="w-full text-xs" color="secondary" value="canJoin" aria-label="centered">
-              참가 가능한 번개
-            </ToggleButton>
-          )
-        }
+        {currentUser && currentUser.data.role !== "guest" && (
+          <ToggleButton
+            className="w-full text-xs"
+            color="secondary"
+            value="canJoin"
+            aria-label="centered"
+          >
+            참가 가능한 번개
+          </ToggleButton>
+        )}
 
-        {
-          currentUser && currentUser.data.role !== "guest" && (
-            <ToggleButton className="w-full text-xs" color="secondary" value="alreadyJoin" aria-label="right aligned">
-              참가 예정인 번개
-            </ToggleButton>
-          )
-        }
+        {currentUser && currentUser.data.role !== "guest" && (
+          <ToggleButton
+            className="w-full text-xs"
+            color="secondary"
+            value="alreadyJoin"
+            aria-label="right aligned"
+          >
+            참가 예정인 번개
+          </ToggleButton>
+        )}
       </ToggleButtonGroup>
 
-      {
-        currentUser && currentUser.data.role !== "guest" && (
-          <div className="flex items-center justify-center">
-            <Link className="w-full text-white no-underline text-center" href={`/events/create?pickDate=${pickDate}`}>
-              <Button
-                variant="gradient"
-                className="mt-4 mx-auto text-center border-none"
-                color="blue"
-                type="button"
-              >
-                번개 생성
-              </Button>
-            </Link>
-          </div>
-        )
-      }
+      {currentUser && currentUser.data.role !== "guest" && (
+        <div className="flex items-center justify-center">
+          <Link
+            className="w-full text-white no-underline text-center"
+            href={`/events/create?pickDate=${pickDate}`}
+          >
+            <Button
+              variant="gradient"
+              className="mt-4 mx-auto text-center border-none cursor-pointer"
+              color="blue"
+              type="button"
+            >
+              번개 생성
+            </Button>
+          </Link>
+        </div>
+      )}
 
       <DateCalendar
         value={pickDate}
@@ -200,38 +226,56 @@ export const CalendarComponent = () => {
         }}
         onChange={(newValue) => {
           setToggleFilter("");
-          setPickDate(newValue)
+          setPickDate(newValue);
           //현재 주소뒤에 query paramter로 pickDate를 붙여준다.
-          window.history.pushState({}, "", `?pickDate=${dayjs(newValue).format('YYYY-MM-DD')}`);
+          window.history.pushState(
+            {},
+            "",
+            `?pickDate=${dayjs(newValue).format("YYYY-MM-DD")}`
+          );
         }}
         onMonthChange={(newValue) => {
-          setPickDate(newValue)
-          setToggleFilter("monthAll")
+          setPickDate(newValue);
+          setToggleFilter("monthAll");
         }}
-        renderLoading={() => <DayCalendarSkeleton />} />
+        renderLoading={() => <DayCalendarSkeleton />}
+      />
 
       <div className="flex flex-col space-y-2">
-        {events && events.map((event: EventProps, index: number) => {
-          return (
-            <Link key={index} href={`/events/${event.id}`} className="no-underline text-black">
-              <Button color={`${event.current_members_count >= event.max_members_count ? "red" : "green"}`} className={`w-full border-none hover:cursor-pointer hover:font-bold hover:opacity-100 opacity-90 drop-shadow py-2 px-4 flex flex-col justify-between mt-2 text-sm rounded-[5px] `}>
-                <div className="flex justify-between w-full">
-                  <div className="flex space-x-4 w-full">
-                    <div>{dayjs(event.start_time.replace(/-/g, "/")).format('YY/MM/DD')}</div>
-                    <div>{event.current_members_count}/{event.max_members_count}</div>
-                    <div>{event.location}</div>
+        {events &&
+          events.map((event: EventProps, index: number) => {
+            return (
+              <Link
+                key={index}
+                href={`/events/${event.id}`}
+                className="no-underline text-black"
+              >
+                <Button
+                  color={`${event.current_members_count >= event.max_members_count ? "red" : "green"}`}
+                  className={`w-full border-none hover:cursor-pointer hover:font-bold hover:opacity-100 opacity-90 drop-shadow py-2 px-4 flex flex-col justify-between mt-2 text-sm rounded-[5px] `}
+                >
+                  <div className="flex justify-between w-full">
+                    <div className="flex space-x-4 w-full">
+                      <div>
+                        {dayjs(event.start_time.replace(/-/g, "/")).format(
+                          "YY/MM/DD"
+                        )}
+                      </div>
+                      <div>
+                        {event.current_members_count}/{event.max_members_count}
+                      </div>
+                      <div>{event.location}</div>
+                    </div>
+                    <div className="shrink-0">{event.owner_name}</div>
                   </div>
-                  <div className="shrink-0">{event.owner_name}</div>
-                </div>
-                <div className="truncate">{event.title}</div>
-              </Button>
-            </Link>
-          )
-        }
-        )}
+                  <div className="truncate">{event.title}</div>
+                </Button>
+              </Link>
+            );
+          })}
       </div>
     </div>
   ) : (
     <LoadingComp />
-  )
-}
+  );
+};
--- app/navbar.tsx
+++ app/navbar.tsx
@@ -1,8 +1,8 @@
-'use client';
+"use client";
 
 import React from "react";
-import onlylogo from '../public/onlylogo.png'
-import MenuIcon from '@mui/icons-material/Menu';
+import onlylogo from "../public/onlylogo.png";
+import MenuIcon from "@mui/icons-material/Menu";
 import {
   Navbar as MTNavbar,
   Collapse,
@@ -11,12 +11,11 @@ import {
   Typography,
 } from "@material-tailwind/react";
 import Image from "next/image";
-import { useEffect, useState } from 'react'
-import { useGetCurrentUser } from './hooks/useGetCurrentUser';
-import Swal from 'sweetalert2';
-import { auth } from '../firebase'
+import { useEffect, useState } from "react";
+import { useGetCurrentUser } from "./hooks/useGetCurrentUser";
+import Swal from "sweetalert2";
+import { auth } from "../firebase";
 import Link from "next/link";
-
 
 const NAV_MENU = [
   {
@@ -43,12 +42,12 @@ const NAV_MENU = [
     accessAll: true,
     targetBlank: true,
   },
-  {
-    name: "보유 보드게임",
-    href: "https://gamromboard.notion.site/a4261df53fc54fc5a403d6a0d8e408e5?pvs=74",
-    accessAll: true,
-    targetBlank: true,
-  },
+  // {
+  //   name: "보유 보드게임",
+  //   href: "https://gamromboard.notion.site/a4261df53fc54fc5a403d6a0d8e408e5?pvs=74",
+  //   accessAll: true,
+  //   targetBlank: true,
+  // },
   {
     name: "회원 관리",
     href: "/admin_page",
@@ -61,14 +60,9 @@ const NAV_MENU = [
     accessAll: false,
     targetBlank: false,
   },
+];
 
-]
-
-function NavItemDiv({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+function NavItemDiv({ children }: { children: React.ReactNode }) {
   return (
     <li>
       <Typography
@@ -81,17 +75,27 @@ function NavItemDiv({
   );
 }
 
-const NavItem = ({ name, href, targetBlank }: { name: string, href: string, targetBlank: boolean }) => {
+const NavItem = ({
+  name,
+  href,
+  targetBlank,
+}: {
+  name: string;
+  href: string;
+  targetBlank: boolean;
+}) => {
   return (
     <NavItemDiv>
-      <a className="no-underline text-black" href={href} target={
-        targetBlank ? "_blank" : ""
-      }>
+      <a
+        className="no-underline text-black"
+        href={href}
+        target={targetBlank ? "_blank" : ""}
+      >
         {name}
       </a>
     </NavItemDiv>
   );
-}
+};
 
 export function Navbar() {
   const [isLogin, setIsLogin] = useState<boolean>(false);
@@ -99,11 +103,10 @@ export function Navbar() {
   const signOut = () => {
     auth.signOut().then(() => {
       setIsLogin(false);
-    })
-  }
+    });
+  };
 
   const { data: currentUser, isLoading: isLoading } = useGetCurrentUser();
-
 
   const [open, setOpen] = React.useState(false);
   const [isScrolling, setIsScrolling] = React.useState(false);
@@ -135,14 +138,20 @@ export function Navbar() {
   const [canSee, setCanSee] = useState<boolean>(false);
   useEffect(() => {
     if (!isLoading && currentUser) {
-      if (currentUser.data.role === "admin" || currentUser.data.role === "manager") {
+      if (
+        currentUser.data.role === "admin" ||
+        currentUser.data.role === "manager"
+      ) {
         setCanSee(true);
       }
     }
-  }, [isLoading, currentUser])
+  }, [isLoading, currentUser]);
 
   return (
-    <nav className="fixed top-0 z-50 border-0 px-4 bg-white w-full py-4" style={{ maxWidth: "-webkit-fill-available" }}>
+    <nav
+      className="fixed top-0 z-50 border-0 px-4 bg-white w-full py-4"
+      style={{ maxWidth: "-webkit-fill-available" }}
+    >
       <div className="container flex items-center justify-between mx-auto">
         <Typography
           as="a"
@@ -153,68 +162,112 @@ export function Navbar() {
           <Image src={onlylogo} alt="logo" width={40} height={40} />
           감롬의 보드게임 동아리
         </Typography>
-        <ul
-          className={`hidden items-center gap-6 lg:flex text-gray-900`}
-        >
+        <ul className={`hidden items-center gap-6 lg:flex text-gray-900`}>
           {NAV_MENU.map((item, index) => {
             if (item.accessAll) {
-              return <NavItem key={`name_${index}`} name={item.name} href={item.href} targetBlank={item.targetBlank} />
+              return (
+                <NavItem
+                  key={`name_${index}`}
+                  name={item.name}
+                  href={item.href}
+                  targetBlank={item.targetBlank}
+                />
+              );
             } else {
               if (canSee) {
-                return <NavItem key={`name_${index}`} name={item.name} href={item.href} targetBlank={item.targetBlank} />
+                return (
+                  <NavItem
+                    key={`name_${index}`}
+                    name={item.name}
+                    href={item.href}
+                    targetBlank={item.targetBlank}
+                  />
+                );
               }
             }
           })}
         </ul>
-        <MenuIcon onClick={handleOpen} className="cursor-pointer h-8 w-8 text-black" />
+        <MenuIcon
+          onClick={handleOpen}
+          className="cursor-pointer h-8 w-8 text-black"
+        />
       </div>
       <Collapse open={open}>
         <div className="container mx-auto bg-white rounded-lg py-4 px-6 mt-3 border-t border-gray-200">
           <ul className="flex flex-col gap-4">
             {NAV_MENU.map((item, index) => {
               if (item.accessAll) {
-                return <NavItem key={`name_${index}`} name={item.name} href={item.href} targetBlank={item.targetBlank} />
+                return (
+                  <NavItem
+                    key={`name_${index}`}
+                    name={item.name}
+                    href={item.href}
+                    targetBlank={item.targetBlank}
+                  />
+                );
               } else {
                 if (canSee) {
-                  return <NavItem key={`name_${index}`} name={item.name} href={item.href} targetBlank={item.targetBlank} />
+                  return (
+                    <NavItem
+                      key={`name_${index}`}
+                      name={item.name}
+                      href={item.href}
+                      targetBlank={item.targetBlank}
+                    />
+                  );
                 }
               }
             })}
           </ul>
           <div className="mt-6 flex items-center gap-2">
-            {!isLoading && (!currentUser ? (
-              <div className="items-center gap-2 lg:flex">
-                <Link href="/login" className="no-underline text-white cursor-pointer" onClick={() => setOpen(false)}>
-                  <Button variant="gradient" color="blue" className="border-none cursor-pointer">
-                    로그인 후 지원하기
+            {!isLoading &&
+              (!currentUser ? (
+                <div className="items-center gap-2 lg:flex">
+                  <Link
+                    href="/login"
+                    className="no-underline text-white cursor-pointer"
+                    onClick={() => setOpen(false)}
+                  >
+                    <Button
+                      variant="gradient"
+                      color="blue"
+                      className="border-none cursor-pointer"
+                    >
+                      로그인 후 지원하기
+                    </Button>
+                  </Link>
+                </div>
+              ) : (
+                <div className="items-center gap-2 lg:flex">
+                  <Button
+                    onClick={() => {
+                      Swal.fire({
+                        title: "로그아웃 하시겠습니까?",
+                        showCancelButton: true,
+                        confirmButtonText: `로그아웃`,
+                        cancelButtonText: `취소`,
+                      })
+                        .then((result) => {
+                          if (result.isConfirmed) {
+                            signOut();
+                          }
+                        })
+                        .finally(() => {
+                          location.reload();
+                        });
+                    }}
+                    variant="filled"
+                    color="red"
+                    className="border-none cursor-pointer"
+                  >
+                    로그아웃
                   </Button>
-                </Link>
-              </div>
-            ) : (
-              <div className="items-center gap-2 lg:flex">
-                <Button onClick={() => {
-                  Swal.fire({
-                    title: '로그아웃 하시겠습니까?',
-                    showCancelButton: true,
-                    confirmButtonText: `로그아웃`,
-                    cancelButtonText: `취소`,
-                  }).then((result) => {
-                    if (result.isConfirmed) {
-                      signOut();
-                    }
-                  }).finally(() => {
-                    location.reload();
-                  })
-                }} variant="filled" color="red" className="border-none cursor-pointer">
-                  로그아웃
-                </Button>
-              </div>
-            ))}
+                </div>
+              ))}
           </div>
         </div>
       </Collapse>
-    </nav >
-
+    </nav>
   );
 }
 
