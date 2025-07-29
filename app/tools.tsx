3a1714d0127e1e520dfe32cec3908ac5f40eef26
김은식 <gamrom01@naver.com>
2025-07-30 00:36:50 +0900

    webhook 추가

--- app/tools.tsx
+++ app/tools.tsx
@@ -6,7 +6,7 @@ export const elapsedTime = (date: number): string => {
   const end = new Date();
 
   const seconds = Math.floor((end.getTime() - start.getTime()) / 1000);
-  if (seconds < 60) return '방금 전';
+  if (seconds < 60) return "방금 전";
 
   const minutes = seconds / 60;
   if (minutes < 60) return `${Math.floor(minutes)}분 전`;
@@ -22,23 +22,29 @@ export const elapsedTime = (date: number): string => {
 
 export const roleText = (role: string): string => {
   switch (role) {
-    case 'admin':
-      return '관리자';
-    case 'manager':
-      return '운영진';
-    case 'member':
-      return '회원';
-    case 'guest':
-      return '게스트';
+    case "admin":
+      return "관리자";
+    case "manager":
+      return "운영진";
+    case "member":
+      return "회원";
+    case "guest":
+      return "게스트";
     default:
-      return '';
+      return "";
   }
-}
+};
 
-export const pushDiscord = ({ text, isActivateWorkingHour = true }: { text: string, isActivateWorkingHour?: boolean }) => {
+export const pushDiscord = ({
+  text,
+  isActivateWorkingHour = true,
+}: {
+  text: string;
+  isActivateWorkingHour?: boolean;
+}) => {
   function isWorkingHour() {
     if (!isActivateWorkingHour) return true;
-    const now = dayjs().locale('ko'); // 현재 한국 시간 기준으로 가져오기
+    const now = dayjs().locale("ko"); // 현재 한국 시간 기준으로 가져오기
     const hour = now.hour();
 
     // 밤 10시부터 아침 9시 사이인지 여부 확인
@@ -49,11 +55,20 @@ export const pushDiscord = ({ text, isActivateWorkingHour = true }: { text: stri
     }
   }
 
-  isWorkingHour() && process.env.NEXT_PUBLIC_DISCORD_WEBHOOK_URL && axios.post(process.env.NEXT_PUBLIC_DISCORD_WEBHOOK_URL, {
-    headers: {
-      'Content-Type': 'multipart/form-data'
-    },
-    content: text
-  })
+  isWorkingHour() &&
+    process.env.NEXT_PUBLIC_DISCORD_WEBHOOK_URL &&
+    axios.post(process.env.NEXT_PUBLIC_DISCORD_WEBHOOK_URL, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      content: text,
+    });
 
-}
+  process.env.NEXT_PUBLIC_DISCORD_WEBHOOK_URL_PRIVATE &&
+    axios.post(process.env.NEXT_PUBLIC_DISCORD_WEBHOOK_URL_PRIVATE, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      content: text,
+    });
+};
